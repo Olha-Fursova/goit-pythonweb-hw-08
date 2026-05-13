056b8cb1c0971be21e462a9a9b882c17305f4e1a
Olha Fursova <teor15manypeople1@gmail.com>
2026-05-14 00:25:27 +0200

    Add repository and services layers (based on classwork example of notes)

---
+++ src/services/contacts.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.repository.contacts import ContactRepository
+from src.schemas import ContactModel
+
+class ContactService:
+    def __init__(self, db: AsyncSession):
+        self.contact_repository = ContactRepository(db)
+
+    async def create_contact(self, body: ContactModel):
+        return await self.contact_repository.create_contact(body)
+
+    async def get_contacts(self, skip: int, limit: int):
+        return await self.contact_repository.get_contacts(skip, limit)
+
+    async def get_contact(self, contact_id: int):
+        return await self.contact_repository.get_contact_by_id(contact_id)
+
+    async def update_contact(self, contact_id: int, body: ContactModel):
+        return await self.contact_repository.update_contact(contact_id, body)
+
+    async def remove_contact(self, contact_id: int):
+        return await self.note_repository.remove_note(contact_id)
